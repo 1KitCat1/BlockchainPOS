140d260ab08deb66ca53bc5d8ad097672fd0d8ca
1KitCat1 <denischool119@gmail.com>
2023-01-17 15:26:25 +0200

    Created transaction class

---
+++ Transaction.py
@@ -0,0 +1,11 @@
+import time
+
+
+class Transaction:
+    def __init__(self, senderPK, receiverPK, amount, transactionType) -> None:
+        self.senderPK = senderPK
+        self.receiverPK = receiverPK
+        self.amount = amount
+        self.transactionType = transactionType
+        self.timestamp = time.time()
+        self.signature = ""
